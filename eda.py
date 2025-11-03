cc31d326bb55a88728d2e0bec963138d49b567cc
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2025-11-03 22:12:13 +0000

    plotting monthly data

--- eda.py
+++ eda.py
@@ -1,7 +1,15 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
 from os import listdir 
 
 fpath = "datasets/"
 files = [f for f in listdir(fpath)]
 
-print(files)
+dataset = files[-1]
+
+df_daily = pd.read_excel(fpath+dataset, sheet_name="Table 1 Daily", header=4) 
+df_monthly = pd.read_excel(fpath+dataset, sheet_name="Table 2 Monthly", header=4) 
+# print(df_monthly.head())
+
+df_monthly.set_index("Date").plot()
+plt.show()

3e5723c3cf5275d3067790b1d20a8e2a30f1df44
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2025-11-05 22:05:13 +0000

    arima model fit and forecast values and visualisation

--- eda.py
+++ eda.py
@@ -27,26 +27,45 @@ df_monthly.set_index("Date", inplace=True)
 # print('p-value: %f' % result[1])
 
 #### Plots ### 
-fig, axs = plt.subplots(3, 2) 
+# fig, axs = plt.subplots(3, 2) 
 
-axs[0, 0].plot(df_monthly)
-axs[0, 0].set_title("Original Series")
-plot_acf(df_monthly.dropna(), ax=axs[0, 1])
+# axs[0, 0].plot(df_monthly)
+# axs[0, 0].set_title("Original Series")
+# plot_acf(df_monthly.dropna(), ax=axs[0, 1])
 
-axs[1, 0].plot(df_monthly.diff())
-axs[1, 0].set_title("1st Order Diff")
-plot_acf(df_monthly.diff().dropna(), ax=axs[1, 1])
+# axs[1, 0].plot(df_monthly.diff())
+# axs[1, 0].set_title("1st Order Diff")
+# plot_acf(df_monthly.diff().dropna(), ax=axs[1, 1])
 
-axs[2, 0].plot(df_monthly.diff().diff())
-axs[2, 0].set_title("2nd Order Diff")
-plot_acf(df_monthly.diff().diff().dropna(), ax=axs[2, 1])
+# axs[2, 0].plot(df_monthly.diff().diff())
+# axs[2, 0].set_title("2nd Order Diff")
+# plot_acf(df_monthly.diff().diff().dropna(), ax=axs[2, 1])
 
-plt.show()
+# plt.show()
 
 
 train = df_monthly[:int(len(df_monthly)*0.75)]
 test = df_monthly[int(len(df_monthly)*0.75):]
 
-# test.plot()
+model = ARIMA(df_monthly, order=(1,1,1))
+fitted = model.fit()
+# print(fitted.summary())
 
-# model = ARIMA(df_monthly, order=(1,1,1))
+forecast = fitted.get_forecast(steps=len(test))
+df = pd.DataFrame(forecast.summary_frame(alpha=0.05))
+# print(df[["mean", "mean_ci_lower"]])
+
+
+fc_series = pd.Series(df["mean"].values, index=test.index)
+lower_series = pd.Series(df["mean_ci_lower"].values, index=test.index)
+upper_series = pd.Series(df["mean_ci_upper"].values, index=test.index)
+
+
+plt.figure(figsize=(15,10), dpi=100)
+plt.plot(train, label="training data")
+plt.plot(test, label="actual data")
+plt.plot(fc_series, label="forecast")
+plt.fill_between(lower_series.index, lower_series, upper_series, color="k", alpha=0.15)
+plt.title("Actual vs Forecast")
+plt.legend(loc="upper left", fontsize=8)
+plt.show() 
